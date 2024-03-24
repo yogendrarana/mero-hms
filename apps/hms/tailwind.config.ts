e6c15d93298b055b5848a4b0ed769e31a71fd191
Yogendra Rana <yogendrarana.tech@gmail.com>
2024-03-24 10:19:24 +0545

    Tailwind config bug fix

--- apps/hms/tailwind.config.ts
+++ apps/hms/tailwind.config.ts
@@ -3,6 +3,5 @@ const base = require("@merohms/config/tailwind/tailwind.config")
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   ...base,
-  content: [...base.content],
-  plugins: [...base.plugins],
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
 }
