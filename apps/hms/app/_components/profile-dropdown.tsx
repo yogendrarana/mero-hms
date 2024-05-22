9decd48d9698c2c48c8e214ba62ab09c19f3f957
Yogendra Rana <yogendrarana.tech@gmail.com>
2024-05-22 22:59:27 +0545

    Added profile dropdown options

--- apps/hms/app/_components/profile-dropdown.tsx
+++ apps/hms/app/_components/profile-dropdown.tsx
@@ -1,38 +1,23 @@
+"use client";
+
 import {
-    Cloud,
-    CreditCard,
-    Github,
-    Keyboard,
-    LifeBuoy,
-    LogOut,
-    Mail,
-    MessageSquare,
-    Plus,
-    PlusCircle,
     Settings,
     User,
-    UserPlus,
-    Users,
+    X,
+    LogOut
 } from "lucide-react"
 import { Button } from "@merohms/ui"
 import {
     DropdownMenu,
     DropdownMenuContent,
-    DropdownMenuGroup,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuPortal,
-    DropdownMenuSeparator,
-    DropdownMenuShortcut,
-    DropdownMenuSub,
-    DropdownMenuSubContent,
-    DropdownMenuSubTrigger,
     DropdownMenuTrigger,
 } from "@merohms/ui"
+import { useState } from "react";
 
 const ProfileDropdown = () => {
+    const [open, setOpen] = useState(false);
     return (
-        <DropdownMenu>
+        <DropdownMenu open={open} onOpenChange={setOpen}>
             <DropdownMenuTrigger asChild>
                 <div className="px-3 py-2 flex gap-2 items-center border rounded-md cursor-pointer">
                     <User size={16} />
@@ -40,84 +25,41 @@ const ProfileDropdown = () => {
                 </div>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end" className="w-[300px] bg-white">
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuGroup>
-                    <DropdownMenuItem>
-                        <User className="mr-2 h-4 w-4" />
-                        <span>Profile</span>
-                        <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                        <CreditCard className="mr-2 h-4 w-4" />
-                        <span>Billing</span>
-                        <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                        <Settings className="mr-2 h-4 w-4" />
-                        <span>Settings</span>
-                        <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                        <Keyboard className="mr-2 h-4 w-4" />
-                        <span>Keyboard shortcuts</span>
-                        <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
-                    </DropdownMenuItem>
-                </DropdownMenuGroup>
-                <DropdownMenuSeparator />
-                <DropdownMenuGroup>
-                    <DropdownMenuItem>
-                        <Users className="mr-2 h-4 w-4" />
-                        <span>Team</span>
-                    </DropdownMenuItem>
-                    <DropdownMenuSub>
-                        <DropdownMenuSubTrigger>
-                            <UserPlus className="mr-2 h-4 w-4" />
-                            <span>Invite users</span>
-                        </DropdownMenuSubTrigger>
-                        <DropdownMenuPortal>
-                            <DropdownMenuSubContent>
-                                <DropdownMenuItem>
-                                    <Mail className="mr-2 h-4 w-4" />
-                                    <span>Email</span>
-                                </DropdownMenuItem>
-                                <DropdownMenuItem>
-                                    <MessageSquare className="mr-2 h-4 w-4" />
-                                    <span>Message</span>
-                                </DropdownMenuItem>
-                                <DropdownMenuSeparator />
-                                <DropdownMenuItem>
-                                    <PlusCircle className="mr-2 h-4 w-4" />
-                                    <span>More...</span>
-                                </DropdownMenuItem>
-                            </DropdownMenuSubContent>
-                        </DropdownMenuPortal>
-                    </DropdownMenuSub>
-                    <DropdownMenuItem>
-                        <Plus className="mr-2 h-4 w-4" />
-                        <span>New Team</span>
-                        <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
-                    </DropdownMenuItem>
-                </DropdownMenuGroup>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                    <Github className="mr-2 h-4 w-4" />
-                    <span>GitHub</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                    <LifeBuoy className="mr-2 h-4 w-4" />
-                    <span>Support</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem disabled>
-                    <Cloud className="mr-2 h-4 w-4" />
-                    <span>API</span>
-                </DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                    <LogOut className="mr-2 h-4 w-4" />
-                    <span>Log out</span>
-                    <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
-                </DropdownMenuItem>
+                <div>
+                    <div className="p-3 flex justify-between items-center">
+                        <h1>Profile</h1>
+
+                        <div className="ml-auto flex gap-1 items-center">
+                            <Button
+                                variant="ghost"
+                                size="icon"
+                                className="p-2 hover:bg-gray-100"
+                                onClick={() => setOpen(false)}
+                            >
+                                <X size={16} />
+                            </Button>
+                        </div>
+                    </div>
+
+                    <div className="p-3 space-y-3 border-t">
+                        <div className="flex gap-3 items-center">
+                            <User size={16} />
+                            <Button variant="ghost" className="p-0">Profile</Button>
+                        </div>
+
+                        <div className="flex gap-3 items-center">
+                            <Settings size={16} />
+                            <Button variant="ghost" className="p-0">Setting</Button>
+                        </div>
+                    </div>
+
+                    <div className="p-3 space-y-3 border-t">
+                        <div className="flex gap-3 items-center cursor-pointer">
+                            <LogOut size={16} />
+                            <Button variant="ghost" className="p-0">Logout</Button>
+                        </div>
+                    </div>
+                </div>
             </DropdownMenuContent>
         </DropdownMenu>
     )
